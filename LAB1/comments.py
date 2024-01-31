e3d0892cdc53a3b3c9a50ec96dd5476210844c66
Assylay <Sar.asylay_2005@mail.ru>
2024-01-31 16:49:06 +0600

    added first sample

---
+++ LAB1/comments.py
@@ -0,0 +1,29 @@
+#task1
+#This is the comment
+
+#task2
+"""This is a comment
+written in 
+more than just one line
+"""
+
+#EXAMPLES
+#This is a comment
+print("Hello, World!")
+
+print("Hello, World!") #This is a comment
+
+#print("Hello, World!")
+print("Cheers, Mate!")
+
+#This is a comment
+#written in
+#more than just one line
+print("Hello, World!")
+
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!")
---
+++ LAB1/syntax.py
@@ -0,0 +1,21 @@
+#1
+print("Helo World")
+
+#2
+if 5 > 2:
+    print("YES")
+
+#ex
+if 5 > 2:
+  print("Five is greater than two!")
+
+if 5 > 2:
+ print("Five is greater than two!") 
+if 5 > 2:
+        print("Five is greater than two!") 
+
+x = 5
+y = "Hello, World!"
+
+#This is a comment.
+print("Hello, World!")
