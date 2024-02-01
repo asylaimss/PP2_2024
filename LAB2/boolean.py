23fe1e77981c08102083eb991ce9b9b04e1d284a
asylaimss <157659038+asylaimss@users.noreply.github.com>
2024-02-01 10:27:30 +0600

    Add files via upload

---
+++ LAB2/boolean.py
@@ -0,0 +1,68 @@
+#examples
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
+
+
+a=220
+b=33
+if b > a:
+    print("b is greater than a")
+else:
+    print("b is not greater than a") 
+
+
+print(bool("Hello"))
+print(bool(15))
+
+
+x = "Hello"
+y = 15
+print(bool(x))
+print(bool(y))
+
+
+bool("abc")
+bool(123)
+bool(["apple", "cherry", "banana"]) #will return True 
+
+
+bool(False)
+bool(None)
+bool(0)
+bool("")
+bool(())
+bool([])
+bool({}) #will return False
+
+
+class myclass():
+  def __len__(self):
+    return 0
+myobj = myclass()
+print(bool(myobj))
+
+
+def myFunction() :
+  return True
+print(myFunction())
+
+
+def myFunction() :
+  return True
+if myFunction():
+  print("YES!")
+else:
+  print("NO!")
+
+
+x = 200
+print(isinstance(x, int))
+
+
+#exercise
+print(10 > 9) #True
+print(10 == 9) #False
+print(10 < 9) #False
+print(bool("abc")) #True
+print(bool(0)) #False
